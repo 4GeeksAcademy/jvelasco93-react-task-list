2768ec8b74e9f532504ade091b44e12b0acc83a5
Julio Cesar Velasco <116952866+jvelasco93@users.noreply.github.com>
2026-06-07 22:47:06 +0000

    Install and update packages and clean project

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
-
-// index.css'
 import '../styles/index.css'
 
-// components
-import Home from './components/Home';
+import App from './components/App';
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+root.render(
   <React.StrictMode>
-    <Home/>
+    <App/>
   </React.StrictMode>,
 )
