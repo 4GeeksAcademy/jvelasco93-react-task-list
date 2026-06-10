01659e8bd8f89d18fa3587727eb561e2fe048e60
Julio <velascojuliocv@gmail.com>
2026-06-10 13:39:26 +0200

    Refactor App component layout and enhance styling with Bootstrap

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
@@ -19,29 +19,49 @@ export default function App() {
   }
 
   return (
-    <>
-      <h1>todo</h1>
-      <TaskInput onAddTask={handleAddTask} />
-      {tasks.length === 0 ? (
-        <p>No task, add a task</p>
-      ) : (
-        <>
-          <ul>
-            {tasks.map((task) => (
-              <TaskItem key={task.id} task={task} onDelete={handleDeleteTask} />
-            ))}
-          </ul>
-          <p>
-            {tasks.length} item{tasks.length !== 1 ? "s" : ""} left
-          </p>
-        </>
-      )}
-    </>
+    <div className="container d-flex flex-column justify-content-start align-items-center mt-3">
+      <div className="card shadow pb-2" style={{ width: "400px" }}>
+        <div className="card-body">
+          <h1
+            className="text-center font-monospace m-0 mb-3 "
+            style={{ fontSize: "5rem", letterSpacing: "-0.125em" }}
+          >
+            todo
+          </h1>
+          <div className="mb-3">
+            <TaskInput onAddTask={handleAddTask} />
+          </div>
+          {tasks.length === 0 ? (
+            <div className="alert alert-warning m-0 py-2">
+              <small>No task, add a task</small>
+            </div>
+          ) : (
+            <ul className="list-group">
+              {tasks.map((task) => (
+                <TaskItem
+                  key={task.id}
+                  task={task}
+                  onDelete={handleDeleteTask}
+                />
+              ))}
+            </ul>
+          )}
+        </div>
+        {tasks.length > 0 && (
+          <div className="card-footer bg-transparent">
+            <small className="text-secondary">
+              {tasks.length} item{tasks.length !== 1 ? "s" : ""} left
+            </small>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
 function TaskInput({ onAddTask }) {
   const [value, setValue] = useState("");
+  const inputRef = useRef(null);
 
   function handleChange(e) {
     setValue(e.target.value);
@@ -52,6 +72,8 @@ function TaskInput({ onAddTask }) {
     if (!trimmed) return;
     onAddTask(trimmed);
     setValue("");
+
+    inputRef.current.focus();
   }
 
   function handleKeyDown(e) {
@@ -61,25 +83,36 @@ function TaskInput({ onAddTask }) {
   }
 
   return (
-    <>
+    <div className="d-flex gap-1">
       <input
+        ref={inputRef}
         type="text"
         value={value}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
+        className="form-control"
+        placeholder="New task..."
       />
-      <button type="button" onClick={submit} disabled>
-        Add task
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        onClick={submit}
+      >
+        Add
       </button>
-    </>
+    </div>
   );
 }
 
 function TaskItem({ task, onDelete }) {
   return (
-    <li className="task-item">
-      <span>{task.description}</span>
-      <button type="button" onClick={() => onDelete(task.id)}>
+    <li className="task-item list-group-item d-flex align-items-center justify-content-between">
+      <span className="me-auto">{task.description}</span>
+      <button
+        type="button"
+        onClick={() => onDelete(task.id)}
+        className="btn btn-outline-danger btn-sm"
+      >
         X
       </button>
     </li>
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import '../styles/index.css'
+//Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-import App from './components/App';
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import "../styles/index.css";
+
+import App from "./components/App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
-)
+);
