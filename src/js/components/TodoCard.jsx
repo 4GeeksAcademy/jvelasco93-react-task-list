643b2814564c49c768a1650b815a6853947ecbd7
Julio <velascojuliocv@gmail.com>
2026-06-12 17:40:11 +0200

    refactor(ui): remove inline css

--- src/js/components/TodoCard.jsx
+++ src/js/components/TodoCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export function TodoCard({ children }) {
   return (
-    <div style={{ width: "400px" }}>
+    <div>
       <div>
-        <h1 style={{ fontSize: "5rem", letterSpacing: "-0.125em" }}>todo</h1>
+        <h1>todo</h1>
         {children}
       </div>
     </div>
